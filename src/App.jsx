fa9ec65861e4b272a817c315b455eadec6067662
ugurhanapaydin <apaydinugurhan@gmail.com>
2023-08-11 18:32:58 +0300

    v1-dondurmaci

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./style";
+
+const App = () =>  (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="sticky `${styles.paddingX} ${styles.flexCenter}`">
+             <div className="`${styles.boxWdth}`">
+            Navbar
+             </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Ücretlendirme
+          cardDeal
+          testimonials
+          clients
+          CTA
+          Footer 
+
+        </div>
+      </div>
+
+
+
+    </div>
+
+    
+  );
+
+
+export default App
